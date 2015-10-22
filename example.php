383458bafeb47f4612cfbbcdec087136bb80895d
Raphaël Dartigues <raphael.dartigues@gmail.com>
2015-10-22 11:23:54 +0200

    Update example.php

--- example.php
+++ example.php
@@ -1,12 +1,12 @@
 <?php
 
 					
-					$form = new_form('get', SCRIPT_URL, 'collect_form', 'navbar-form center');
+	$form = new_form('get', SCRIPT_URL, 'collect_form', 'navbar-form center');
 
-					append_form(get_input_text('twt_add_interest','Your text'),$form);
+	append_form(get_input_text('twt_add_interest','Your text'),$form);
 
-					append_form(get_input_select('period', array('yesterday' => 'selected','today'=>''),'font1 btn-lg btn-default'),$form);
+	append_form(get_input_select('period', array('yesterday' => 'selected','today'=>''),'font1 btn-lg btn-default'),$form);
 
-					append_form(get_input_hidden('token','qsfgjkqsdfhgjksdfkjgh',$form);
+	append_form(get_input_hidden('token','qsfgjkqsdfhgjksdfkjgh',$form);
 
-					echo $form;
+	echo $form;
